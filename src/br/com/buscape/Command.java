a163a1998bfc7abf8835d12976b8b5ab24503ca1
viniciusbaptistel <viniciusbaptistel@192.168.0.3>
2017-09-07 14:23:46 -0300

    Enumarador para comandos de movimento de um Submarine

--- src/br/com/buscape/Command.java
+++ src/br/com/buscape/Command.java
@@ -1,8 +1,18 @@
 package br.com.buscape;
 
+/**
+ * Enumerador para comandos de movimento de um {@link Submarine}
+ * 
+ * @author viniciusbaptistel
+ *
+ */
 public enum Command {
 
-	LEFT('L'), RIGHT('R'), MOVE('M'), UP('U'), DOWN('D');
+	LEFT('L'), 
+	RIGHT('R'), 
+	MOVE('M'), 
+	UP('U'), 
+	DOWN('D');
 
 	private final char value;
 
@@ -14,6 +24,13 @@ public enum Command {
 		return value;
 	}
 
+	/**
+	 * Busca um comando pelo valor
+	 * 
+	 * @param value - char
+	 * @return {@link Command}
+	 * @throws RuntimeException se comando não encontrado
+	 */
 	public static Command getByValue(char value) {
 		for (Command cmd : values()) 
 			if (cmd.value == value) 
